8140955db51971895c399ed1aec772c59d94f030
Luca Scheller <luca.scheller@gmail.com>
2023-07-09 13:04:47 +0200

    Improve docs build script

--- docs.sh
+++ docs.sh
@@ -10,6 +10,7 @@ rm -R ${REPO_ROOT}/docs/book
 # $HFS/python/bin/python -m venv ${REPO_ROOT}/tools/python
 # source ${REPO_ROOT}/tools/python/bin/activate
 # python -m pip install pydoc-markdown
+# This is currently not being used as it pydoc-markdown fails to detect all docstrings.
 # Install 
 # Build book
 mdbook serve --open ${REPO_ROOT}/docs
