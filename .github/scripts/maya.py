cfeb4910a1dd7eb099aa71bc1aaa075d8db1aa11
Luca Scheller <luca.scheller@gmail.com>
2024-03-20 22:12:09 -0700

    Fix python interpreter of tests

--- .github/scripts/maya.py
+++ .github/scripts/maya.py
@@ -179,11 +179,11 @@ def install_autodesk_product(product, version, install_dir_path):
         os.chmod(python_configure_file_path, 0o777)
         logging.info("Configuring Python Build")
         command = [python_configure_file_path, "--enable-shared", "--prefix", python_install_dir_path]
-        process = subprocess.check_call(command,  stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=python_version_dir_path)
+        process = subprocess.check_call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=python_version_dir_path)
         logging.info("Building Python")
         process = subprocess.check_call(["make"], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=python_version_dir_path)
         logging.info("Installing Python")
-        process = subprocess.check_call(["make", "install"],  stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=python_version_dir_path)
+        process = subprocess.check_call(["make", "install"], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=python_version_dir_path)
         # Maya USD SDK
         maya_usd_sdk_releases = get_autodesk_maya_usd_sdk_releases(autodesk_platform, autodesk_maya_version)
         maya_usd_sdk_latest_release = maya_usd_sdk_releases[0]
@@ -202,7 +202,7 @@ def install_autodesk_product(product, version, install_dir_path):
         # command = ["rpm", "-e", maya_usd_sdk_extract_rpm_file_name.replace(".rpm", "")]
         # process = subprocess.check_call(command, cwd=maya_usd_sdk_extract_dir_path)
         command = ["rpm", "-i", "--nodeps","--prefix", maya_usd_sdk_extract_dir_path, maya_usd_sdk_extract_rpm_file_path]
-        process = subprocess.check_call(command, cwd=maya_usd_sdk_extract_dir_path)
+        process = subprocess.check_call(command, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=maya_usd_sdk_extract_dir_path)
         maya_usd_sdk_extract_usd_dir_path = glob.glob("{root_dir}{sep}**{sep}mayausd{sep}USD".format(root_dir=maya_usd_sdk_extract_dir_path, sep=os.path.sep), recursive=True)[0]
         maya_usd_sdk_extract_usd_dir_path = os.path.join(maya_usd_sdk_extract_dir_path, maya_usd_sdk_extract_usd_dir_path)
         os.rename(maya_usd_sdk_extract_usd_dir_path, maya_usd_sdk_install_dir_path)
